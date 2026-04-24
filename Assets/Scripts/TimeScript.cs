5ca32c879faf7eb433923733a2ea10bbeaef6f0d
MAETEL\2247041 <kouseichuanben@gmail.com>
2026-04-24 16:39:22 +0900

    時間減少と指定秒ごとに減らす時間倍増

--- Assets/Scripts/TimeScript.cs
+++ Assets/Scripts/TimeScript.cs
@@ -7,23 +7,24 @@ public class TimeScript : MonoBehaviour
     public float LimitTime = 30f;        // 残り時間
     public float Downspeed = 1f;        // 減少速度
     private float elapsedTime = 0f; // 経過時間
+    private float ReduceLimit=30f;   //減らす時間
 
     public TMP_Text timeText;
 
     void Update()
     {
         // 時間の減少
-        LimitTime -= Time.deltaTime;
+        LimitTime -= Time.deltaTime*Downspeed;
 
         // 経過時間を加算
         elapsedTime += Time.deltaTime;
 
         // 30秒ごとに減少速度を0.1追加
-        if (elapsedTime >= 30f)
+        if (elapsedTime >= ReduceLimit)
         {
             Downspeed += 0.1f;
-            LimitTime -= Downspeed;
             elapsedTime = 0f;
+            Debug.Log(Downspeed.ToString("F1"));
         }
         if(LimitTime < 0f) 
         {
