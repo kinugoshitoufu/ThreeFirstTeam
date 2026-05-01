55f4d739d0f7335a80c66aa36cc408b106615c65
MAETEL\2247041 <kouseichuanben@gmail.com>
2026-05-01 15:24:03 +0900

    経過時間の追加

--- Assets/Scripts/GameSystem/TimeScript.cs
+++ Assets/Scripts/GameSystem/TimeScript.cs
@@ -8,7 +8,8 @@ public class TimeScript : MonoBehaviour
     public static TimeScript instance;
     public float LimitTime = 30f;     // 残り時間
     public float Downspeed = 1f;     // 減少速度
-    private float elapsedTime = 0f;  // 経過時間
+    private float ElapsedDownTime = 0f;  // 指定時間ごとに減らす経過時間
+    private float ElapsedTime = 0f;  // ゲーム開始から始まる経過時間
     private float ReduceLimit = 30f;   //減らす時間
 
     public float speed = 0.1f;  //回転速度
@@ -38,15 +39,16 @@ public class TimeScript : MonoBehaviour
         LimitTime -= Time.deltaTime*Downspeed;
 
         // 経過時間を加算
-        elapsedTime += Time.deltaTime;
+        ElapsedDownTime += Time.deltaTime;
 
         // 30秒ごとに減少速度を0.1追加
-        if (elapsedTime >= ReduceLimit)
+        if (ElapsedDownTime >= ReduceLimit)
         {
             Downspeed += 0.1f;
-            elapsedTime = 0f;
+            ElapsedDownTime = 0f;
             Debug.Log(Downspeed.ToString("F1"));
         }
+
         if(LimitTime <= 0f && !isFinished) 
         {
             isFinished = true;
