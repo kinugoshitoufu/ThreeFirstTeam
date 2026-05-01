409737e5b67819eab2dae684badf1831203dc014
MAETEL\2247041 <kouseichuanben@gmail.com>
2026-05-01 14:20:54 +0900

    残り時間による画像の切り替え

--- Assets/Scripts/GameSystem/TimeScript.cs
+++ Assets/Scripts/GameSystem/TimeScript.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TimeScript : MonoBehaviour
@@ -10,12 +11,25 @@ public class TimeScript : MonoBehaviour
     private float elapsedTime = 0f;  // 経過時間
     private float ReduceLimit = 30f;   //減らす時間
 
+    public float speed = 0.1f;  //回転速度
+    public Image redImage;    //0～60
+    public Image yellowImage; //61～120
+    public Image blueImage;   //121～180
 
     public TMP_Text timeText;
 
     private bool isFinished = false;
     void Update()
     {
+        if (Input.GetKey(KeyCode.T))
+        {
+            LimitTime += 2;
+        }
+        if (Input.GetKey(KeyCode.Y))
+        {
+            LimitTime -= 2;
+        }
+
         // 時間の減少
         LimitTime -= Time.deltaTime*Downspeed;
 
@@ -33,7 +47,6 @@ public class TimeScript : MonoBehaviour
         {
             isFinished = true;
 
-
             int finalScore = PlayerScript.instance.GetScore();
             Debug.Log("終了時スコア：" + finalScore);
 
@@ -49,8 +62,27 @@ public class TimeScript : MonoBehaviour
             LimitTime = 0f;//０以下にならないように
         }
 
-
         // 整数表示（秒）
         timeText.text = Mathf.FloorToInt(LimitTime).ToString("F0");
+
+        // 一旦全部OFF
+        redImage.gameObject.SetActive(false);
+        yellowImage.gameObject.SetActive(false);
+        blueImage.gameObject.SetActive(false);
+
+        // 条件ごとに1つだけON
+        if (LimitTime <= 60f)
+        {
+            redImage.gameObject.SetActive(true);
+        }
+        else if (LimitTime <= 120f)
+        {
+            yellowImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            blueImage.gameObject.SetActive(true);
+        }
+
     }
 }
