7abbaddfd35ed8dcda640fcbd7a08f70d52ea598
K2_PC_VERSION21\K2_ko <kouseichuanben@gmail.com>
2026-05-17 00:42:02 +0900

    コンボ表示(なんかあった時の保険用プル)

--- Assets/Scripts/Combo.cs
+++ Assets/Scripts/Combo.cs
@@ -4,16 +4,62 @@ using UnityEngine.UI;
 
 public class Combo : MonoBehaviour
 {
-    public TextMeshProUGUI ComboText;//コンボのテキスト参照
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TextMeshProUGUI evencombotext;   // even=偶数
+    public TextMeshProUGUI oddcombotext;  // odd=奇数
+
+    public PlayerScript playerscript;
+
+    private int combo;      //現時点のコンボ
+    private int oldcombo;   //検知用のコンボ
+
+    private Vector2 evenposx;
+    private Vector2 oddposx;
+
     void Start()
     {
-        
-    }
+        // ワールド座標
+        evenposx = evencombotext.rectTransform.anchoredPosition;
+        oddposx = oddcombotext.rectTransform.anchoredPosition;
 
-    // Update is called once per frame
+        // 初期表示
+        evencombotext.text = "0";
+        oddcombotext.text = "0";
+
+        oldcombo = 0;
+    }
     void Update()
     {
-        
+        ComboCount();
+    }
+
+    void ComboCount()
+    {
+        //コンボ取得
+        combo = playerscript.GetMaxComboCount();
+
+        // コンボが増えたら
+        if (combo > oldcombo)
+        {
+            Vector2 temp = evencombotext.rectTransform.anchoredPosition;
+
+            evencombotext.rectTransform.anchoredPosition =
+                oddcombotext.rectTransform.anchoredPosition;
+
+            oddcombotext.rectTransform.anchoredPosition = temp;
+
+            // 偶数
+            if (combo % 2 == 0)
+            {
+                evencombotext.text = combo.ToString();
+            }
+            // 奇数
+            else
+            {
+                oddcombotext.text = combo.ToString();
+            }
+
+            // 前回値更新
+            oldcombo = combo;
+        }
     }
 }
