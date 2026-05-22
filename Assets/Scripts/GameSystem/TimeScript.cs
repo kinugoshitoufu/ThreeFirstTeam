06f103f680607f9dc162e1e0b40d29a6ff4866ea
MAETEL\2247041 <kouseichuanben@gmail.com>
2026-05-22 17:17:52 +0900

    時間をテキストに変更(一旦小数点だい2まで)、テキスト参照は新しくテキストメッシュプロ作って貰って

--- Assets/Scripts/GameSystem/TimeScript.cs
+++ Assets/Scripts/GameSystem/TimeScript.cs
@@ -11,18 +11,13 @@ public class TimeScript : MonoBehaviour
     private float elapsedDownTime = 0f;  // 指定時間ごとに減らす経過時間
     private float elapsedTime = 0f;  // ゲーム開始から始まる経過時間
     public float eeduceLimit = 30f;   //減らす時間
-
     public float speed = 0.1f;  //回転速度
-    public Image redImage;    //0～60
-    public Image yellowImage; //61～120
-    public Image blueImage;   //121～180
-
+    public TextMeshProUGUI timetext;
 
     //吉本追加
     public float upTecreaseTime = 0.2f;//○○秒おきに減る時間スピードを早くさせるか
     public GameObject timeUpEffect;
 
-    private bool isResultShown = false;
     private bool isFinished = false;
     public static bool stopTimer = false;
     void Start()
@@ -31,21 +26,6 @@ public class TimeScript : MonoBehaviour
     }
     void Update()
     {
-        //if (Input.GetKey(KeyCode.Return))
-        //{
-        //    if (Input.GetKey(KeyCode.K))
-        //    {
-        //        limitTime = 130;
-        //    }
-        //    if (Input.GetKey(KeyCode.Y))
-        //    {
-        //        limitTime = 70;
-        //    }
-        //    if (Input.GetKey(KeyCode.U))
-        //    {
-        //        limitTime = 2;
-        //    }
-        //}
 
         // 時間の減少
         if (!stopTimer) limitTime -= Time.deltaTime * downspeed*speed;
@@ -61,14 +41,11 @@ public class TimeScript : MonoBehaviour
             downspeed += upTecreaseTime;
             elapsedDownTime = 0f;
             Debug.Log(downspeed.ToString("F1"));
-
         }
 
         if (limitTime <= 0f && !isFinished)
         {
             isFinished = true;
-
-            isResultShown = true;
 
 
             int finalScore = PlayerScript.instance.GetScore();
@@ -79,55 +56,12 @@ public class TimeScript : MonoBehaviour
             ResultScript.resultCombo = PlayerScript.instance.maxcombo;
             FadeManager.instance.StartFade();
             ResultScript.instance.ShowResult();
-            //PlayerScript.instance.isFalling = true;
             return;
         }
         if (limitTime < 0)
         {
             limitTime = 0f;//０以下にならないように
         }
-
-        // 表示
-        redImage.gameObject.SetActive(true);
-        yellowImage.gameObject.SetActive(true);
-        blueImage.gameObject.SetActive(true);
-        redImage.fillAmount = 1f;
-        yellowImage.fillAmount = 1f;
-        blueImage.fillAmount = 1f;
-
-        if (limitTime > 180f)
-        {
-            limitTime = 180f;//120以上行かないように
-        }
-        else if (limitTime > 120f) // 青180～121
-        {           
-            float t = Mathf.InverseLerp(180f, 121f, limitTime);
-            blueImage.fillAmount = 1f - t;
-        }
-        else if (limitTime > 60f) // 黄色120～61
-        {
-            yellowImage.gameObject.SetActive(true);
-            blueImage.gameObject.SetActive(false);
-            float t = Mathf.InverseLerp(120f, 61f, limitTime);
-            yellowImage.fillAmount = 1f - t;
-        }
-        else // 赤60～0
-        {
-            blueImage.gameObject.SetActive(false);
-            yellowImage.gameObject.SetActive(false);
-            float t = Mathf.InverseLerp(60f, 0f, limitTime);
-            redImage.fillAmount = 1f - t;
-        }
-
-    }
-
-    public void UpEffect()
-    {
-        Vector3 screenPos = redImage.rectTransform.position;
-
-        Vector3 worldPos =Camera.main.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, 10f));
-        worldPos.z = 0;
-
-        Instantiate(timeUpEffect, worldPos, Quaternion.identity);
+        timetext.text = limitTime.ToString("F2");
     }
 }
